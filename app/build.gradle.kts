0886f80488e50534d9baccbf9364323211f91d36
whazami <wl.hazami@gmail.com>
2026-08-01 23:13:05 +0200

    minSdk 36 -> 30 (LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.crispim.recentapps"
-        minSdk = 36
+        minSdk = 30
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
